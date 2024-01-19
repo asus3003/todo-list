695e13e0a8010dc739080c6b394baf2615c510ee
Alex Usenko <alexx3003@ya.ru>
2024-01-19 08:17:39 +0300

    add index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+let addMessage = document.querySelector('.message'), addButton = document.querySelector('.add');
+
+addButton.addEventListener('click', function (){
+//каждое новое дело будем создавать в объект, а объекты будем помещать в массив
+    //создаем массив newtodo. Массив будет создаваться, когда мы будем добавлять новое сообщение. Он будет содержать данные нашего последнего сообщения. 
+let newTodo = {
+    todo:addMessage.value,
+}
+
+
+
+
+
+    console.log(addMessage.value);
+})
