7dea793d0b1101c388cfce7154a350c7c9f41eca
Alex Usenko <alexx3003@ya.ru>
2024-01-24 17:11:37 +0300

    created addEventListener for Every Click

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let formInputEl = document.querySelector('.form-input'),
-    formEl = document.querySelector('form'),
+    // formEl = document.querySelector('form'),
     formButtonEl = document.querySelector('.form-button'),
     todoListEl = document.querySelector('.todo-list');
+
+let clickButton = document.querySelector('html');
+
 const name = 'todo';
 let todoList = [];
 
 checkLocalStorage(name);
 
- //formEl.addEventListener('keypress', clearDefault);
+//formEl.addEventListener('keypress', clearDefault);
+
+clickButton.addEventListener('click',(e) => everyClickButton(e));
 
 formButtonEl.addEventListener('click', () => onSubmit(name));
 
-formInputEl.addEventListener('keydown', onEnter);
+formInputEl.addEventListener('keydown', onKeyPress);
 
 todoListEl.addEventListener('change', changeTodoList);
 
@@ -34,16 +39,15 @@ function checkLocalStorage(todoName) {
     }
 }
 
-function onEnter(e) {
-   // console.log('key',e);
-    if (e.ctrlKey || e.metaKey || e.key === 'Enter') {
+function onKeyPress(e) {
+    // console.log('key',e);
+    if (e.ctrlKey || e.metaKey) {
         onSubmit(name);
     }
 }
 
 function onSubmit(todoName) {
     if (!formInputEl.value) return;
-
 
     let newTodo = {
         todo: formInputEl.value,
@@ -66,7 +70,7 @@ function displayMessages() {
     todoList.forEach(function (item, i) {
         displayMessage += `
         <li>
-        <input type='checkbox' id='item_${i}'${item.checked ? 'checked' : ''}>
+        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
         <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
         </li>
         `;
@@ -79,7 +83,8 @@ function changeTodoList(event) {
     let idInput = event.target.getAttribute('id');
     let forLabel = todoListEl.querySelector('[for=' + idInput + ']');
     let valueLabel = forLabel.innerHTML;
-    todoList.forEach(function (item) {
+    todoList.forEach((item) => {
+        // console.log('change',this);
         if (item.todo === valueLabel) {
             item.checked = !item.checked;
             localStorage.setItem('todo', JSON.stringify(todoList));
@@ -90,6 +95,7 @@ function changeTodoList(event) {
 function contextTodoList(event) {
     event.preventDefault();
     todoList.forEach(function (item, i) {
+        // console.log('context',this);
         if (item.todo === event.target.innerHTML) {
             if (event.ctrlKey || event.metaKey) {
                 todoList.splice(i, 1);
@@ -103,3 +109,13 @@ function contextTodoList(event) {
     });
 }
 
+function everyClickButton (e) {
+    console.log(e.target);
+}
+// document.addEventListener('click', (e)=>{
+//     console.log(e.target);
+//     if (клик по нужному элементу-Х или по всему что в нём){
+//         произвести некоторые действия
+//     }
+// })
+// if (e.target.closest('.className')) {}
